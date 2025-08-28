d895b527268dbfbded4eb3149b4f376927c8eccc
SapphireCascade <codeychase64@gmail.com>
2025-08-28 16:46:37 +0100

    update routes

--- routes.php
+++ routes.php
@@ -1,6 +1,6 @@
 <?php
     require_once "vendor/autoload.php";
     use Dandylion\Route;
-    Route::route("/posts","posts.php");
     Route::route("/users","users.php");
+    Route::route("/posts","posts.php");
     Route::route("/users/{id}","user.php");
